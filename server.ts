b8bd65f068893932a6c54bded8de1648e8bb2a19
Inbestigator <inbestigatoryt@gmail.com>
2024-12-05 17:26:15 -0800

    Updated bundler

--- server.ts
+++ server.ts
@@ -1,12 +1,3 @@
-import { bundle } from "@libs/bundle/ts/bundle";
-
-Deno.writeTextFileSync(
-  "./bundle.js",
-  await bundle(Deno.readTextFileSync("main.ts"), {
-    minify: "terser",
-  }),
-);
-
 Deno.serve((req) => {
   if (new URL(req.url).pathname === "/bundle.js") {
     return new Response(Deno.readTextFileSync("bundle.js"));
